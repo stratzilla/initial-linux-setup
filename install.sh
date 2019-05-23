3327a4455b54a531901a52c317bfe67fe204a5cb
Robin Scott <stratzillab@gmail.com>
2019-05-23 19:53:35 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get --yes --force-yes install xorg openbox
+apt --yes --force-yes install gnat-8
+apt --yes --force-yes install python3
+apt --yes --force-yes install gcc
+apt --yes --force-yes install g++
+apt-get --yes --force-yes install freeglut3-dev
+apt-get --yes --force-yes install libfreeimage3 libfreeimage-dev
+apt-get --yes --force-yes install python3-tk
+apt-get --yes --force-yes install imagemagick
