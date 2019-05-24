924c63d68d0ae539430912fd2542d010c32f787f
Robin Scott <stratzillab@gmail.com>
2019-05-23 20:29:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-apt-get --yes --force-yes install xorg openbox
-apt --yes --force-yes install gnat-8
-apt --yes --force-yes install python3
-apt --yes --force-yes install gcc
-apt --yes --force-yes install g++
-apt-get --yes --force-yes install freeglut3-dev
-apt-get --yes --force-yes install libfreeimage3 libfreeimage-dev
-apt-get --yes --force-yes install python3-tk
-apt-get --yes --force-yes install imagemagick
+apt --yes --force-yes install xorg openbox
+apt --yes --force-yes install gnat-8 gcc g++
+apt --yes --force-yes install python3 python3-tk
+apt --yes --force-yes install freeglut3-dev libfreeimage3 libfreeimage-dev imagemagick
