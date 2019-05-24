17ea6c06f9276993ed00e8e11d1b4727938cd88b
Robin Scott <stratzillab@gmail.com>
2019-05-23 21:37:56 -0400

    Update lxss-setup.sh

--- lxss-setup.sh
+++ lxss-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd /root || exit
+cd /root || exit # go to root dir
+# update package installer
 apt update -y && apt upgrade -y
-mkdir -p /mnt/c/linux
-echo "cd /mnt/c/linux" >> /root/.bashrc
-echo "export DISPLAY=:0.0" >> /root/.bashrc
+mkdir -p /mnt/c/linux # lxss root dir
+echo "cd /mnt/c/linux" >> /root/.bashrc # make default dir
+echo "export DISPLAY=:0.0" >> /root/.bashrc # default X11 display
