0559174c9d2e87789192c057dcf50fc9a3f5ff51
Robin Scott <stratzillab@gmail.com>
2019-06-07 20:00:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 apt install -y xorg openbox # X11 window manager
 apt install -y gcc g++ gnat-8 # c, c++, ada, others
 apt install -y python3 python3-tk # python
+apt install pythonpy
 apt install -y default-jre scala # java, scala
 # opengl/glut, freeimage, imagemagick
 apt install -y freeglut3-dev libfreeimage3 libfreeimage-dev imagemagick
+py -m pip install Pillow # PIL
